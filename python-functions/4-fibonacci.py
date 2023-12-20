cd5003ff22a0fe5e38fa60306a3ab5c286bc02d3
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2023-12-20 22:05:12 +0100

    task4

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,7 +2,7 @@ def fibonacci_sequence(n):
     fibonacci =[0,1]
     while len(fibonacci) < n:
         next_numb = fibonacci[-1]+ fibonacci[-2]
-        fibonacci = n + next_numb
+        fibonacci = fibonacci + [next_numb]
     return fibonacci [:n]
    
 
