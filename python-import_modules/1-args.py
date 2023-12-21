d3012e714122f6eb188700128703ed6b334b165e
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2023-12-21 16:49:50 +0100

    task 2

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,17 @@
+import sys
+
+def print_arguments():
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+    output_length = 0
+
+    if num_arguments > 0:
+        print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:", end='\n')
+        for i, arg in enumerate(arguments, start=1):
+            print(f"{i}: {arg}")
+            output_length += len(f"{i}: {arg}\n")
+    else:
+          print(f"{num_arguments} arguments.")
+
+if __name__ == "__main__":
+    print_arguments()
