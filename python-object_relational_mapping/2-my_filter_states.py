d2485eb830bfc785c25c74ade523d6d326c6e5d0
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-02-11 10:41:27 +0100

    TASK2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ config_connect = Server.connect(host="localhost", port=3306, user=argv[1], passw
 
 cursor = config_connect.cursor()
 
-cursor.execute("SELECT * FROM states WHERE name ='{}' ORDER BY id ASC".format(argv(4)))
+cursor.execute("SELECT * FROM states WHERE name ='{}' ORDER BY id ASC".format(argv[4]))
 
 rows = cursor.fetchall()
 
