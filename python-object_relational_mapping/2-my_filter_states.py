1f7f4799ec62586ec3ae32d49e68c76e95a58b44
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-02-11 10:35:33 +0100

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+
+
+import MySQLdb as Server
+from sys import argv
+
+config_connect = Server.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], database=argv[3])
+
+cursor = config_connect.cursor()
+
+cursor.execute("SELECT * FROM states WHERE name ='{}' ORDER BY id ASC".format(argv(4)))
+
+rows = cursor.fetchall()
+
+for row in rows:
+    if row[1][0]=='N':
+     print(row)
+
+cursor.close()
+config_connect.close()
+
