93dd8dff85695e921b20d9eb14c6149964043ea9
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-02-13 12:35:33 +0100

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,23 @@
 import MySQLdb as Server
 from sys import argv
 
-config_connect = Server.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], database=argv[3])
+config_connect = Server.connect(host='localhost', port=3306, user=argv[1], password=argv[2], db=argv[3])
 
 cursor = config_connect.cursor()
 
-cursor.execute('''SELECT cities.id, cities.name, states.name FROM cities
-                JOIN states ON cities.state_id = states.id
-                ORDER BY cities.state_id ASC, cities.id ASC;
-                ''')
+query = '''SELECT cities.name FROM cities
+          JOIN states ON cities.state_id = states.id
+          WHERE state_id = (SELECT id FROM states WHERE name = %s)
+          ORDER BY cities.id ASC'''
+
+
+cursor.execute(query, (argv[4],))
 
 rows = cursor.fetchall()
 
-for row in rows:
-  print(row)
+result = ', '.join(row[0] for row in rows)
+print(result)
+
 
 cursor.close()
 config_connect.close()
