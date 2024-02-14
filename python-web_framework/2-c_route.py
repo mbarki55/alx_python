9982c035752220bbaaf527a9d7ac8931ce24cf29
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-02-14 16:55:57 +0100

    task 2

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,21 @@
+"""A simple Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb',strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
