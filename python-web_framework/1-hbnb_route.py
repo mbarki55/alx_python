5832212cc202158e0a807576b447a56e1c489878
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-02-14 16:47:17 +0100

    task 1

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,17 @@
+"""A simple Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/')
+def hbnb():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
