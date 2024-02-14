5f33d1c389b567ca791d66768cf1b5c152080afd
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-02-14 19:40:53 +0100

    task 5

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,44 +1,41 @@
-"""A simple Flask web application."""
 from flask import Flask
+import re
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_hbnb():
+    """Route to greet the user with "Hello HBNB!"."""
     return 'Hello HBNB!'
 
-
-@app.route('/hbnb',strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
+    """Route to display "HBNB"."""
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """Route to display "C" followed by the provided text."""
     return 'C ' + text.replace('_', ' ')
 
-'''Route to display "Python" followed by the provided text'''
-
-@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/', defaults={'text': 'is cool'})
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
+    """Route to display "Python" followed by the provided text."""
     return 'Python ' + text.replace('_', ' ')
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return f'{n} is a number'
 
-
-# Route to display "Number: n" where n is an integer
-
 @app.route('/number_template/<int(n)>', strict_slashes=False)
 def number_template(n):
+    """Route to display 'Number: n' where n is an integer."""
     return f'<html><body><h1>Number: {n}</h1></body></html>'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
 
 
-    
-    
+
